71c319d7bb4c9c854d4384132b25940cbc487a9a
Yogesh Khasturi <34720956+yogeshkhasturi@users.noreply.github.com>
2022-01-05 14:49:25 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @category   Webiators
+* @package    Webiators_Root
+* @author     Webiators Team
+* @copyright  Copyright (c) Webiators Technologies Private Limited. ( https://store.webiators.com ).
+*/
+\Magento\Framework\Component\ComponentRegistrar::register(
+	\Magento\Framework\Component\ComponentRegistrar::MODULE,
+	'Webiators_Root',
+	__DIR__
+);
